91f5b11901ab0dadea581e7f53f5c8e966f8a172
rca <nem@nem.x0.to>
2024-11-14 10:55:38 +0900

    サブパスで正しく動くように？

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
     host: '0.0.0.0',
     port: 3030
   },
+  app: {
+    baseURL: '/booth-util/',
+  },
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3000/api'
